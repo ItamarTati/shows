54578b02ba152bc282759e16ba6818bd4dec157e
ItamarTati <itamar.tati1994@gmail.com>
2023-03-21 16:45:57 +0000

    specifying output directory

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
     rollupOptions: {
       external: ['react-dom/client'],
     },
