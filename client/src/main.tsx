f2f8908cf97dd0f884e346da9d81a93165b98d94
ItamarTati <itamar.tati1994@gmail.com>
2023-03-21 19:06:56 +0000

    some quality changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
+
