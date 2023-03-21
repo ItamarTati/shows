9c845aeb9baed9213db36ab97750f7e9260c0e93
ItamarTati <itamar.tati1994@gmail.com>
2023-03-21 18:45:15 +0000

    updated code for your server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/dist/index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
   });
 }
 
