f394159af7d2644efb9b0e7803e7bb3e585cfb65
ItamarTati <itamar.tati1994@gmail.com>
2023-03-21 18:48:25 +0000

    fixing prod

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use(helmet.contentSecurityPolicy({
 
 app.use(cors());
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/dist"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
   });
 }
 
