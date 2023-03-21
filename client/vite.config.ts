b919904f2d9e799bb439a09792d1214e9e1186b6
ItamarTati <itamar.tati1994@gmail.com>
2023-03-21 16:53:42 +0000

    dist

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'client/index',
+    outDir: '../dist',
     rollupOptions: {
       external: ['react-dom/client'],
     },
