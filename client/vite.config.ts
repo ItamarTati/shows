4cb254b321892720ec5aafdb3e54f12483db37c1
ItamarTati <itamar.tati1994@gmail.com>
2023-03-21 16:15:59 +0000

    changed config again

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
       external: ['react-dom/client'],
     },
   },
-})
+});
